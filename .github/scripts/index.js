6e49e0ff5b6ff269a745a0dd479e0b6c28a733ae
Hugo Bernier <hugoabernier#live.ca>
2024-04-14 14:45:42 -0400

    Extracted join URL

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -31,46 +31,15 @@ async function fetchICS() {
     }
 }
 
-function convertToUTC(dateString) {
-    // Split the date string into the time zone and the date parts
-    const [tzid, datePart] = dateString.split(':');
-
-    // Get the time zone name
-    const timeZone = tzid.split('=')[1];
-
-    // Map the time zone name to an IANA time zone
-    const timeZoneMap = {
-        'Eastern Standard Time': 'America/New_York',
-        // Add more mappings as needed
-    };
-    const ianaTimeZone = timeZoneMap[timeZone];
-
-    // Parse the date and time parts from the date string
-    const year = parseInt(datePart.slice(0, 4), 10);
-    const month = parseInt(datePart.slice(4, 6), 10);
-    const day = parseInt(datePart.slice(6, 8), 10);
-    const hour = parseInt(datePart.slice(8, 10), 10);
-    const minute = parseInt(datePart.slice(10, 12), 10);
-    const second = parseInt(datePart.slice(12, 14), 10);
-
-    // Create a new DateTime object in the specified time zone
-    const dateTime = DateTime.fromObject({ year, month, day, hour, minute, second, zone: ianaTimeZone });
-
-    // Convert the DateTime object to UTC
-    const utcDateTime = dateTime.toUTC();
-
-    return utcDateTime.toISO();
-}
-
 function parseAndConvertICALToJSON(icsData) {
     const jcal = ical.parse(icsData);
     const comp = new ical.Component(jcal);
     const events = comp.getAllSubcomponents('vevent');
     const mappedEvents = events.map(event => {
         const vevent = new ical.Event(event);
-        if (!vevent.location ) {
-            vevent.location = "Microsoft Teams Meeting";
-        }
+        const meetingUrl = extractMeetingUrlFromDescription(vevent.description);
+        console.log(`Meeting URL: ${meetingUrl}`); // Outputs the meeting URL or "null" if not found
+
         const rruleProp = vevent.component.getFirstProperty('rrule');
         const rrule = rruleProp ? rruleProp.getFirstValue() : null;
         // Try to fetch RECURRENCE-ID directly
@@ -131,8 +100,9 @@ function parseAndConvertICALToJSON(icsData) {
         return {
             uid: vevent.uid,
             summary: vevent.summary,
+            joinUrl: meetingUrl,
             location: vevent.location ? vevent.location.toString() : "Microsoft Teams Meeting",
-            description: vevent.description,
+            joinUrl: meetingUrl,
             startTime: vevent.startDate.toString(),
             endTime: vevent.endDate.toString(),
             rrule: rrule ? rrule.toString() : null,
@@ -149,11 +119,24 @@ function parseAndConvertICALToJSON(icsData) {
             const matchingEvent = mappedEvents.find(event => event.uid === eventWithRecurrenceId.uid);
 
             if (matchingEvent) {
-                // Add the recurrence date to the matching event's nextOccurrences array
-                matchingEvent.nextOccurrences.push({
-                    date: eventWithRecurrenceId.startTime.toString(),
-                    status: "moved"
-                });
+                // Convert the event with recurrenceId start time to UTC
+                const yearPart = eventWithRecurrenceId.recurrenceId.slice(0, 4);
+                const monthPart = eventWithRecurrenceId.recurrenceId.slice(4, 6);
+                const dayPart = eventWithRecurrenceId.recurrenceId.slice(6, 8);
+                const utcDate = `${yearPart}-${monthPart}-${dayPart}`;
+
+
+                // Find the matching event's nextOccurrences array by matching the date portion only
+                const matchingOccurrence = matchingEvent.nextOccurrences.find(occurrence => occurrence.date.startsWith(utcDate  ));   
+
+                if (matchingOccurrence) {
+                    // Change the status of the matching occurrence to moved
+                    matchingOccurrence.status = "moved";
+                    matchingOccurrence.date = eventWithRecurrenceId.startTime.toString();
+                    matchingOccurrence.summary = eventWithRecurrenceId.summary;
+                    matchingOccurrence.description = eventWithRecurrenceId.description;
+                }
+  
             }
         }
     });
@@ -174,75 +157,34 @@ function parseExdates(vevent) {
     });
 }
 
-function getNextOccurrences(vevent, dateAfter) {
-    try {
-        const exdates = parseExdates(vevent).map(date => date.toISOString());
-        const rruleProp = vevent.component.getFirstProperty('rrule');
-        let occurrences = [];
-        const ruleString = rruleProp ? rruleProp.getFirstValue().toString() : null;
-        if (rruleProp) {
-            const fixedRruleString = fixRecurrenceRule(ruleString);
-            //const rule = RRule.fromString(`DTSTART:${vevent.startDate.toJSDate()};\nRRULE:${fixedRruleString}`);
-            const ruleSetString = `RRULE:${fixedRruleString}`;
-
-            const rule = RRule.fromString(ruleSetString);
-
-            var ruleSet = new RRuleSet();
-            ruleSet.rrule(rule);
-            ruleSet.startDate = vevent.startDate;
-
-            const dateBefore = new Date(dateAfter); // Current date
-            dateBefore.setFullYear(dateAfter.getFullYear() + 1); // 1 year from now
-
-
-            const nextDates = rule.between(dateAfter, dateBefore, true, (date, i) => {
-                return i < 7
-            });
-
-            nextDates.forEach(next => {
-                // fix the date to match the start date
-                next.setHours(vevent.startDate.hour, vevent.startDate.minute, vevent.startDate.second);
-
-                occurrences.push({
-                    date: next.toISOString(),
-                    status: "scheduled"
-                });
-            });
-
-            exdates.forEach(exdate => {
-                let index = occurrences.findIndex(occ => occ.date === exdate);
-                if (index !== -1) {
-                    occurrences[index].status = "cancelled"; // Change status to cancelled
-                } else {
-                    occurrences.push({
-                        date: exdate,
-                        status: "cancelled"
-                    });
-                }
-            });
-
-            occurrences.sort((a, b) => new Date(a.date) - new Date(b.date));
-        }
-        return occurrences;
-    } catch (error) {
-        console.error("Error in getNextOccurrences:", error);
-        return [];
-    }
+function extractMeetingUrlFromDescription(description) {
+    const regex = /\n\nClick here to join the meeting<([^>]+)>\n\n_/;
+    const match = description.match(regex);
+    return match ? match[1] : null;
 }
 
-
-function fixRecurrenceRule(rruleString) {
-    return rruleString.replace(/BYDAY=(\d)([A-Z]+)/g, 'BYDAY=+$1$2');
-}
 
 async function main() {
     ensureDirectoryExists(ICS_OUTPUT_FILE);
 
-    const icsData = await fetchICS();
+    let icsData = await fetchICS();
     if (!icsData) {
         console.log('Failed to download ICS data. Exiting.');
         return;
     }
+
+    // Add Location if not present
+
+    // Search and replace every occurrence of a line with only LOCATION: with LOCATION:Microsoft Teams Meeting
+
+    icsData = icsData.replace(/^LOCATION:$/gm, 'LOCATION:Microsoft Teams Meeting');
+
+    // Find this:
+    // X-MICROSOFT-REQUESTEDATTENDANCEMODE:DEFAULT
+    // And replace with this:
+    // X-MICROSOFT-REQUESTEDATTENDANCEMODE:ONLINE
+    icsData = icsData.replace(/X-MICROSOFT-REQUESTEDATTENDANCEMODE:DEFAULT/g, 'X-MICROSOFT-REQUESTEDATTENDANCEMODE:ONLINE');
+
 
     fs.writeFileSync(ICS_OUTPUT_FILE, icsData);
     console.log('ICS file has been downloaded and saved.');
