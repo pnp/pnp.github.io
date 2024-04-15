11711a8693a93541f0fe75800e22d03f1d2ae938
Hugo Bernier <hugoabernier@live.ca>
2024-04-15 08:35:07 -0400

    Updated scripts

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -1,10 +1,10 @@
 const fetch = require('node-fetch');
-const { ICAL } = require('ical.js');
+const ical = require('ical.js');
 const fs = require('fs');
 const path = require('path');
 
 const ICS_URL = process.env.ICS_URL; // Use the environment variable
-const DIR_PATH = '../../ical'; // Adjusted to point to the 'ical' directory at the root
+const DIR_PATH = '../../ical';
 const ICS_OUTPUT_FILE = path.join(DIR_PATH, 'calendar.ics');
 const JSON_OUTPUT_FILE = path.join(DIR_PATH, 'calendar.json');
 
@@ -22,8 +22,7 @@ async function fetchICS() {
         if (!response.ok) {
             throw new Error(`Failed to fetch ICS file: ${response.statusText}`);
         }
-        const data = await response.text();
-        return data;
+        return await response.text();
     } catch (error) {
         console.error('Fetch ICS Error:', error);
         return null;
@@ -31,60 +30,185 @@ async function fetchICS() {
 }
 
 function parseAndConvertICALToJSON(icsData) {
-    try {
-        const jcal = ICAL.parse(icsData);
-        const comp = new ICAL.Component(jcal);
-        const events = comp.getAllSubcomponents('vevent');
-        return events.map(event => {
-            const vevent = new ICAL.Event(event);
-            return {
-                summary: vevent.summary,
-                location: vevent.location,
-                description: vevent.description,
-                startTime: vevent.startDate.toString(),
-                endTime: vevent.endDate.toString(),
-                rrule: vevent.component.getFirstPropertyValue('rrule') ? vevent.component.getFirstPropertyValue('rrule').toString() : null,
-                exdate: parseExdates(vevent),
-                recurrenceId: vevent.component.getFirstPropertyValue('recurrence-id') ? vevent.component.getFirstPropertyValue('recurrence-id').toString() : null
-            };
+    const jcal = ical.parse(icsData);
+    const comp = new ical.Component(jcal);
+    const events = comp.getAllSubcomponents('vevent');
+    const mappedEvents = events.map(event => {
+        const vevent = new ical.Event(event);
+        const meetingUrl = extractMeetingUrlFromDescription(vevent.description);
+        
+        // Add the meeting URL as a custom property
+        if (meetingUrl) {
+            event.addPropertyWithValue('x-microsoft-skypeteamsmeetingurl', meetingUrl);
+        }
+
+        const rruleProp = vevent.component.getFirstProperty('rrule');
+        const rrule = rruleProp ? rruleProp.getFirstValue() : null;
+        // Try to fetch RECURRENCE-ID directly
+        const recurrenceIdProp = vevent.component.getFirstProperty('recurrence-id');
+        const recurrenceId = recurrenceIdProp ? recurrenceIdProp.getFirstValue().toICALString() : null;
+
+        console.log(`Event: ${vevent.summary}`); // Debugging statement
+
+        const todayAtMidnight = new Date();
+        todayAtMidnight.setHours(0, 0, 0, 0);
+        let nextOccurrences = []; //getNextOccurrences(vevent, todayAtMidnight);
+
+
+        let expand = new ical.RecurExpansion({
+            component: event,
+            dtstart: event.getFirstPropertyValue('dtstart')
         });
-    } catch (error) {
-        console.error('Error converting ICAL to JSON:', error);
-        return null;
-    }
+
+        // next is always an ICAL.Time or null
+        let next;
+
+        let i = 0;
+        while (i < 7 && (next = expand.next())) {
+
+            const jsDate = next.toJSDate();
+            const isoDate = jsDate.toISOString();
+
+            const today = new Date();
+            today.setHours(0, 0, 0, 0); // Set the time to midnight
+
+            if (jsDate > today) {
+                nextOccurrences.push({
+                    date: isoDate,
+                    status: "scheduled"
+                });
+                i++;
+            } 
+        
+        }
+
+        // Go through the exception dates and mark them as cancelled
+        const exdates = parseExdates(vevent);
+        exdates.forEach(exdate => {
+            let index = nextOccurrences.findIndex(occ => occ.date === exdate);
+            if (index !== -1) {
+                nextOccurrences[index].status = "cancelled"; // Change status to cancelled
+            } else {
+                nextOccurrences.push({
+                    date: exdate,
+                    status: "cancelled"
+                });
+            }
+        });
+
+        // sort the next occurrences by date
+        nextOccurrences.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+        return {
+            uid: vevent.uid,
+            summary: vevent.summary,
+            joinUrl: meetingUrl,
+            location: vevent.location ? vevent.location.toString() : "Microsoft Teams Meeting",
+            joinUrl: meetingUrl,
+            startTime: vevent.startDate.toJSDate().toISOString(),
+            endTime: vevent.endDate.toJSDate().toISOString(),
+            rrule: rrule ? rrule.toString() : null,
+            exdate: exdates,
+            recurrenceId: recurrenceId ? recurrenceId.toString() : null,
+            nextOccurrences: nextOccurrences
+        };
+    });
+
+    // After the events array has been created...
+    mappedEvents.forEach(eventWithRecurrenceId => {
+        if (eventWithRecurrenceId.recurrenceId) {
+            // Find the event with a matching UID
+            const matchingEvent = mappedEvents.find(event => event.uid === eventWithRecurrenceId.uid);
+
+            if (matchingEvent) {
+                // Convert the event with recurrenceId start time to UTC
+                const yearPart = eventWithRecurrenceId.recurrenceId.slice(0, 4);
+                const monthPart = eventWithRecurrenceId.recurrenceId.slice(4, 6);
+                const dayPart = eventWithRecurrenceId.recurrenceId.slice(6, 8);
+                const utcDate = `${yearPart}-${monthPart}-${dayPart}`;
+
+
+                // Find the matching event's nextOccurrences array by matching the date portion only
+                const matchingOccurrence = matchingEvent.nextOccurrences.find(occurrence => occurrence.date.startsWith(utcDate  ));   
+
+                if (matchingOccurrence) {
+                    // Change the status of the matching occurrence to moved
+                    matchingOccurrence.status = "moved";
+                    matchingOccurrence.date = eventWithRecurrenceId.startTime.toString();
+                    matchingOccurrence.summary = eventWithRecurrenceId.summary;
+                    matchingOccurrence.description = eventWithRecurrenceId.description;
+                }
+  
+            }
+        }
+    });
+
+    // After all events have been processed, convert the Component back to an ICS string
+    const updatedIcsData = comp.toString();
+
+    return { mappedEvents, updatedIcsData };
 }
 
 function parseExdates(vevent) {
     const exdates = vevent.component.getAllProperties('exdate');
-    return exdates.map(exdate => exdate.getFirstValue().toString());
+    return exdates.map(exdate => {
+        // Assuming exdate is in UTC and converting accordingly
+        const jsDate = exdate.getFirstValue().toJSDate();
+        const isoDate = jsDate.toISOString();
+
+        return isoDate;
+    });
 }
 
+function extractMeetingUrlFromDescription(description) {
+    const regex = /\n\nClick here to join the meeting<([^>]+)>\n\n_/;
+    const match = description.match(regex);
+    return match ? match[1] : null;
+}
+
+
 async function main() {
-    try {
-        ensureDirectoryExists(ICS_OUTPUT_FILE);
+    ensureDirectoryExists(ICS_OUTPUT_FILE);
 
-        const icsData = await fetchICS();
-        if (!icsData) {
-            console.log('Failed to download ICS data. Exiting.');
-            return;
-        }
+    let icsData = await fetchICS();
+    if (!icsData) {
+        console.log('Failed to download ICS data. Exiting.');
+        return;
+    }
 
-        fs.writeFileSync(ICS_OUTPUT_FILE, icsData);
-        console.log('ICS file has been downloaded and saved.');
+    // Add Location if not present
 
-        const jsonData = {
-            lastRetrieved: new Date().toISOString(),
-            events: parseAndConvertICALToJSON(icsData)
-        };
+    // Search and replace every occurrence of a line with only LOCATION: with LOCATION:Microsoft Teams Meeting
 
-        if (jsonData.events) {
-            fs.writeFileSync(JSON_OUTPUT_FILE, JSON.stringify(jsonData, null, 2));
-            console.log('ICS data has been converted to JSON and saved.');
-        } else {
-            console.log('Failed to convert ICAL to JSON or JSON is empty.');
-        }
-    } catch (error) {
-        console.error('Error processing ICS file:', error);
+    icsData = icsData.replace(/^LOCATION:$/gm, 'LOCATION:Microsoft Teams Meeting');
+
+    // Find this:
+    // X-MICROSOFT-REQUESTEDATTENDANCEMODE:DEFAULT
+    // And replace with this:
+    // X-MICROSOFT-REQUESTEDATTENDANCEMODE:ONLINE
+    icsData = icsData.replace(/X-MICROSOFT-REQUESTEDATTENDANCEMODE:DEFAULT/g, 'X-MICROSOFT-REQUESTEDATTENDANCEMODE:ONLINE');
+
+
+    fs.writeFileSync(ICS_OUTPUT_FILE, icsData);
+    console.log('ICS file has been downloaded and saved.');
+
+    const { mappedEvents, updatedIcsData } = parseAndConvertICALToJSON(icsData);
+
+    // Save the updated ICS data to a file
+    fs.writeFileSync(ICS_OUTPUT_FILE, updatedIcsData);
+    console.log('Updated ICS file has been saved.');
+
+    const jsonData = {
+        lastRetrieved: new Date().toISOString(),
+        events: mappedEvents
+    };
+
+    if (jsonData.events) {
+        fs.writeFileSync(JSON_OUTPUT_FILE, JSON.stringify(jsonData, null, 2));
+        console.log('ICS data has been converted to JSON and saved.');
+        console.log('The file has been saved at: ', path.resolve(JSON_OUTPUT_FILE));
+    } else {
+        console.log('Failed to convert ICAL to JSON or JSON is empty.');
     }
 }
 
